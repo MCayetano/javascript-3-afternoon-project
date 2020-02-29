65904de8fe9316984a11bd54d64c0d0c9893f7cc
Marco <marcocayetanojr@gmail.com>
2020-02-28 19:05:21 -0700

    destructuring done

--- destructuring.js
+++ destructuring.js
@@ -109,8 +109,14 @@ function largeNumbers({first, second, third}){
 */
 
 function numberGroups({a, b, c}){
-  
-
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else {
+    return c;
+  }
 }
+
 
 
